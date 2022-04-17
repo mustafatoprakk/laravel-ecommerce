753ac69d2e605e404a920e1a0489d8e12ea0e79e
mustafa toprak <mmustafa.ttoprak@gmail.com>
2022-04-18 02:36:36 +0300

    user controller ve middleware oluşturuldu ve middleware ile session işlemi yapıldı

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -6,12 +6,13 @@
         <div class="col-md-6 offset-md-3 mt-5">
             <div class="card mt-5">
                 <div class="card-body">
-                    <form action="">
+                    <form action="{{ route('login') }}" method="POST">
+                        @csrf
                         <div class="mb-3">
-                            <input class="form-control form-control-lg" type="email" placeholder="Email">
+                            <input name="email" id="email" class="form-control form-control-lg" type="email" placeholder="Email">
                         </div>
                         <div class="mb-3">
-                            <input class="form-control form-control-lg" type="password" placeholder="Password">
+                            <input name="password" id="password" class="form-control form-control-lg" type="password" placeholder="Password">
                         </div>
                         <div class="d-grid gap-2">
                             <button type="submit" class="btn btn-primary">Login</button>
