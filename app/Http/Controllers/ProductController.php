ed3170425251f5ece1484d5bb7c71027d578a2a0
mustafa toprak <mmustafa.ttoprak@gmail.com>
2022-04-18 04:01:15 +0300

    home page slider yapıldı

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -14,7 +14,8 @@ class ProductController extends Controller
      */
     public function index()
     {
-        return "hoşgeldin";
+        $products = Product::all();
+        return view("product", compact("products"));
     }
 
     /**
