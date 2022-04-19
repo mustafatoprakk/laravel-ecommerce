001e2b6be804e06a64ee4e97c61a4914bfca419d
mustafa toprak <mmustafa.ttoprak@gmail.com>
2022-04-19 14:09:24 +0300

    order sayfası eksikleri giderildi

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -18,7 +18,7 @@ if(Session::has('user')){
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
           <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="/">Home</a>
+            <a class="nav-link active" aria-current="page" href="{{ route('home-page') }}">Home</a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="#">Orders</a>
--- resources/views/order.blade.php
+++ resources/views/order.blade.php
@@ -4,33 +4,55 @@
 
 <div class="container">
     <div class="row">
-        <div class="col-md-4 offset-md-4" style="margin-top: 10%">
+        <div class="col-md-4 offset-md-4" style="margin-top: 6%">
             <form action="" method="post">
-                <div class="card">
-                    <div class="card-body text-center">
-                        <h4 class="card-title fw-bold">Order Summary</h4>
-                        <div class="row mt-4 border-bottom">
-                            <div class="col-md-6">
-                                <p class="font-monospace">Total</p>
+                <div class="card shadow p-3 mb-5 bg-body rounded">
+                    <div class="card-body">
+                        <h4 class="card-title fw-bold text-center">Order Summary</h4>
+                        <div class="row mt-4">
+                            <div class="col-md-12">
+                                <div class="row">
+                                    <div class="col-md-6">
+                                        <p class="font-monospace">Subtotal</p>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <p class="font-monospace float-end">
+                                            {{ $total }} TL
+                                        </p>
+                                    </div>
+                                </div>
                             </div>
-                            <div class="col-md-6 float-end">
-                                <p class="font-monospace">
-                                    {{ $total }}
-                                </p>
+                            <div class="col-md-12">
+                                <div class="row">
+                                    <div class="col-md-6">
+                                        <p class="font-monospace">Delivery</p>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <p class="font-monospace float-end">
+                                            12 TL
+                                        </p>
+                                    </div>
+                                </div>
                             </div>
-                        </div>
-                        <div class="mt-4">
-                            <button type="submit" class="btn btn-success">Complete the Order</button>                        
-                        </div>
-                        <div class="mt-3">
-                            <a href="{{ route('cartList') }}" class="text-decoration-none">Product Detail</a>
+                            <div class="col-md-12">
+                                <div class="row">
+                                    <div class="col-md-6">
+                                        <p class="font-monospace">Total</p>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <p class="font-monospace float-end">
+                                            {{ $total + 12 }} TL
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
-                <div class="card mt-3">
+                <div class="card shadow p-3 mb-5 bg-body rounded">
                     <div class="card-body">
                         <div class="mb-3">
-                            <input type="email" class="form-control" id="emial" name="email" placeholder="name@example.com">
+                            <textarea class="form-control" id="address" name="address" rows="3" placeholder="Address"></textarea>
                         </div>
                         <div class="form-check">
                             <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
@@ -38,11 +60,17 @@
                               Online payment
                             </label>
                         </div>
-                        <div class="form-check">
+                        <div class="form-check border-bottom pb-4">
                             <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" checked>
                             <label class="form-check-label" for="flexRadioDefault2">
                               Payment on Delivery
                             </label>
+                        </div>
+                        <div class="mt-4 text-center">
+                            <button type="submit" class="btn btn-success">Complete the Order</button>                        
+                        </div>
+                        <div class="mt-3 text-center">
+                            <a href="{{ route('cartList') }}" class="text-decoration-none">Product Detail</a>
                         </div>
                     </div>
                 </div>
